700bab8b2d0f4e239d419b7ae8e5639a6a065db2
Rob <hello@robertmartin.dev>
2025-05-27 19:35:48 -0400

    Add buyer profile with collection

---
+++ app/data/store.ts
@@ -0,0 +1,52 @@
+export type Vinyl = {
+  id: string;
+  title: string;
+  artist: string;
+  year: number;
+};
+
+export type BuyerProfile = {
+  name: string;
+  bio: string;
+  collection: Vinyl[];
+};
+
+const defaultProfile: BuyerProfile = {
+  name: "Vinyl Aficionado",
+  bio: "A lover of all things analog.",
+  collection: [
+    { id: "1", title: "Kind of Blue", artist: "Miles Davis", year: 1959 },
+    { id: "2", title: "Abbey Road", artist: "The Beatles", year: 1969 },
+  ],
+};
+
+let profile: BuyerProfile = { ...defaultProfile };
+
+export async function getProfile() {
+  return profile;
+}
+
+export async function updateProfile(data: Partial<BuyerProfile>) {
+  profile = { ...profile, ...data };
+}
+
+export async function addVinyl(vinyl: Omit<Vinyl, "id">) {
+  const id = Date.now().toString();
+  profile.collection.push({ ...vinyl, id });
+}
+
+export type Interest = {
+  vinylId: string;
+  name: string;
+};
+
+const interests: Interest[] = [];
+
+export async function expressInterest(vinylId: string, name: string) {
+  interests.push({ vinylId, name });
+  console.log(`Interest from ${name} for vinyl ${vinylId}`);
+}
+
+export async function getInterests() {
+  return interests;
+}

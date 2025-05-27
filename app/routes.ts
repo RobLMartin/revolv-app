4f6c4854ef210d441cbadd7415f07577f6fa80a0
Rob Martin <rmartin3@alumni.unca.edu>
2025-05-27 19:30:30 -0400

    fix route

--- app/routes.ts
+++ app/routes.ts
@@ -1,6 +1,6 @@
-import { type RouteConfig, index, route } from "@react-router/dev/routes";
+import { index, route, type RouteConfig } from "@react-router/dev/routes";
 
 export default [
   index("routes/home.tsx"),
-  route("routes/vendors.$vendorId.tsx"),
+  route("vendors/:vendorId", "routes/vendors.$vendorId.tsx"),
 ] satisfies RouteConfig;
